cf5c7375149bc67243aa81de5f767a2a02970024
Ryan Ramage <r@mage.rocks>
2025-02-19 12:10:01 -0700

    make url use lastSeq

--- client/impl/sugar/watch.mjs
+++ client/impl/sugar/watch.mjs
@@ -6,6 +6,7 @@ import { WatchDocs } from '../../schema/sugar/watch.mjs'
 
 // watch the doc for any changes
 export const watchDocs = WatchDocs.implement((config, docIds, onChange, options = {}) => {
+  let lastSeq = null || 'now'
   const logger = createLogger(config)
   const feed = 'continuous'
   const includeDocs = options.include_docs ?? false
@@ -13,7 +14,7 @@ export const watchDocs = WatchDocs.implement((config, docIds, onChange, options 
   if (_docIds.length === 0) throw new Error('docIds must be a non-empty array')
   if (_docIds.length > 100) throw new Error('docIds must be an array of 100 or fewer elements')
   const ids = _docIds.join('","')
-  const url = `${config.couch}/_changes?feed=${feed}&since=now&include_docs=${includeDocs}&filter=_doc_ids&doc_ids=["${ids}"]`
+  const url = `${config.couch}/_changes?feed=${feed}&since=${lastSeq}&include_docs=${includeDocs}&filter=_doc_ids&doc_ids=["${ids}"]`
 
   const opts = {
     headers: { 'Content-Type': 'application/json' },
@@ -23,7 +24,6 @@ export const watchDocs = WatchDocs.implement((config, docIds, onChange, options 
   const emitter = new EventEmitter()
   let buffer = ''
   const req = needle.get(url, opts)
-  let lastSeq = null
   let stopping = false
 
   req.on('data', chunk => {
